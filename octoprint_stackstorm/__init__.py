5310576a587f3622adb3841d61887c9b53d37ab7
James Fryman <james@fryman.io>
2015-10-30 16:28:23 -0500

    more event handling

--- octoprint_stackstorm/__init__.py
+++ octoprint_stackstorm/__init__.py
@@ -8,27 +8,46 @@ import requests
 
 class StackStormPlugin(octoprint.plugin.SettingsPlugin,
                        octoprint.plugin.TemplatePlugin,
+                       octoprint.plugin.StartupPlugin,
                        octoprint.plugin.EventHandlerPlugin):
 
     ## SettingsPlugin
 
     def get_settings_defaults(self):
-        return dict(webhook_url="", api_key="")
+        return dict(
+            webhook_url="",
+            api_key=""
+        )
+
+    def on_settings_save(self, data):
+        old_webhook_url = self._settings.get(['webhook_url'])
+        old_api_key = self._settings.get(['api_key'])
+
+        octoprint.plugin.SettingsPlugin.on_settings_save(self, data)
+        new_webhook_url = self._settings.get(['webhook_url'])
+        new_api_key = self._settings.get(['api_key'])
+
+        if old_webhook_url != new_webhook_url:
+            self._logger.info("StackStorm Webhook URL changed from {old_webhook_url} to {new_webhook_url}".format(**locals()))
+
+        if old_api_key != new_api_key:
+            self._logger.info("StackStorm API Key changed")
 
     def get_settings_version(self):
         return 1
 
     ## TemplatePlugin
-
     def get_template_configs(self):
         return [dict(type="settings", name="StackStorm", custom_bindings=False)]
+
+    ## StartupPlugin
+    def on_after_startup(self):
+        webhook_url = self._settings.get(['webhook_url'])
+        api_key = self._settings.get(['api_key'])
 
     ## EventPlugin
 
     def on_event(self, event, payload):
-        webhook_url = self._settings.get(['webhook_url'])
-        api_key = self._settings.get(['api_key'])
-
         if webhook_url == "":
             self._logger.exception("StackStorm Webhook URL not set!")
             return
